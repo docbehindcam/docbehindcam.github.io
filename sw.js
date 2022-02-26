7bcd255301ab69790aef1a84453efb82e31d72c2
docbehindcam <100272040+docbehindcam@users.noreply.github.com>
2022-02-26 12:08:35 +0530

    new_products_seo_description

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220226.1320';
+const cacheName = 'chirpy-20220226.1436';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
